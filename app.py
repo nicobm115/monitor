c4b02f3e0069bf3e566266d320a49dd7abf509c7
nicobm115 <nicodarg007@gmail.com>
2025-12-16 13:09:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def get_wind_style(knots):
     return "#cc1b76", "#000000"             # Violeta (Temporal)
 
 def get_cardinal(deg):
-    dirs = ["N",  "NE",  "E", "SE",  "S",  "SO", , "O", , "NO", ]
+    dirs = ["N",  "NE",  "E", "SE",  "S",  "SO",  "O",  "NO", ]
     ix = int((deg + 11.25)/22.5)
     return dirs[ix % 8]
 
@@ -217,6 +217,7 @@ if data:
                 )
 else:
     st.error("Error conectando con MeteoGalicia.")
+
 
 
 
